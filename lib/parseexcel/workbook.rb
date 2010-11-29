5b401fd7ab15183f16feb6c2c3034d596ec0b89e
Pat Allan <pat@freelancing-gods.com>
2010-11-29 11:10:11 +1100

    Adding accessor for worksheets to Workbook, switching tabs to spaces, and removing sheet_count method - accessor covers this neatly enough.

--- lib/parseexcel/workbook.rb
+++ lib/parseexcel/workbook.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
-#	Spreadsheet::ParseExcel -- Extract Data from an Excel File
-#	Copyright (C) 2003 ywesee -- intellectual capital connected
+#  Spreadsheet::ParseExcel -- Extract Data from an Excel File
+#  Copyright (C) 2003 ywesee -- intellectual capital connected
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,65 +17,73 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-#	ywesee - intellectual capital connected, Winterthurerstrasse 52, CH-8006 Zürich, Switzerland
-#	hwyss@ywesee.com
+#  ywesee - intellectual capital connected, Winterthurerstrasse 52, CH-8006 Zürich, Switzerland
+#  hwyss@ywesee.com
 #
 # Workbook -- Spreadsheet::ParseExcel -- 10.06.2003 -- hwyss@ywesee.com 
 
 require 'parseexcel/worksheet'
 
 module Spreadsheet
-	module ParseExcel
-		class Annotation < String
-			attr_accessor :author
-		end
-		class Workbook
-			attr_accessor :biffversion, :version, :flg_1904, :fonts
-			attr_writer :format
-			def initialize
-				@worksheets = []
-				@pkg_strs = []
-				@annotations = []
-				@formats = []
-  			@fonts = []
-  		end
-			def add_annotation(pkg_str)
-				@annotations.push(pkg_str)
-			end
-			def add_text_format(idx, fmt_str)
-				@format.add_text_format(idx, fmt_str)
-			end
-			def add_cell_format(format)
-				@formats.push(format)
-			end
-	    def add_font(font_str)
-    		@fonts.push(font_str)
-    	end
-  		def add_pkg_str(pkg_str)
-				@pkg_strs.push(pkg_str)
-			end
-			def annotation(idx)
-				@annotations.at(idx)
-			end
-			def format(idx=nil)
-				(idx.nil?) ? @format : @formats.at(idx)
-			end
-			def font(idx)
-				@fonts.at(idx)
-			end
-			def pkg_str(idx)
-				@pkg_strs.at(idx)
-			end
-			def sheet_count
-				@worksheets.size
-			end
+  module ParseExcel
+    class Annotation < String
+      attr_accessor :author
+    end
+    class Workbook
+      attr_accessor :biffversion, :version, :flg_1904, :fonts, :worksheets
+      attr_writer :format
+      
+      def initialize
+        @worksheets = []
+        @pkg_strs = []
+        @annotations = []
+        @formats = []
+        @fonts = []
+      end
+      
+      def add_annotation(pkg_str)
+        @annotations.push(pkg_str)
+      end
+      
+      def add_text_format(idx, fmt_str)
+        @format.add_text_format(idx, fmt_str)
+      end
+      
+      def add_cell_format(format)
+        @formats.push(format)
+      end
+      
+      def add_font(font_str)
+        @fonts.push(font_str)
+      end
+      
+      def add_pkg_str(pkg_str)
+        @pkg_strs.push(pkg_str)
+      end
+      
+      def annotation(idx)
+        @annotations.at(idx)
+      end
+      
+      def format(idx=nil)
+        (idx.nil?) ? @format : @formats.at(idx)
+      end
+      
+      def font(idx)
+        @fonts.at(idx)
+      end
+      
+      def pkg_str(idx)
+        @pkg_strs.at(idx)
+      end
+      
       def worksheet(idx, encoding=nil)
         if(idx.is_a?(Integer))
           @worksheets[idx] ||= Worksheet.new
         else
           @worksheets.find { |sheet| sheet.name(encoding) == idx }
         end
-			end
-		end
-	end
+      end
+    end
+  end
 end
